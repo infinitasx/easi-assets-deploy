ffcee691d4d87206e366164076868c7762582bf0
Chris Chen <me.chenying@gmail.com>
2021-05-14 10:38:23 +0800

    feat: 修改changlog文档参考地址

--- scripts/postInstall.js
+++ scripts/postInstall.js
@@ -120,7 +120,7 @@ if (cwd.indexOf('node_modules') >= 0) {
       [
         '# 变更日志',
         '本项目的所有更改都将记录在此文件中。',
-        '本文件格式基于 [Keep a Changelog](https://keepachangelog.com/en/1.0.0/) ，' +
+        '本文件格式基于 [Keep a Changelog](https://keepachangelog.com/zh-CN/1.0.0/) ，' +
         '并且遵循 [Semantic Versioning](https://semver.org/spec/v2.0.0.html) 。',
         os.EOL
       ].join(os.EOL + os.EOL))
